ed3f2f5d6a7b946461107387fdbfca676fe1e9b1
ASHISH <ashishsharma@beusalons.com>
2020-05-09 21:00:39 +0530

    api

--- app/src/main/java/com/app/alphasucess/service/NetworkServiceLayer.java
+++ app/src/main/java/com/app/alphasucess/service/NetworkServiceLayer.java
@@ -2,19 +2,22 @@ package com.app.alphasucess.service;
 import com.app.alphasucess.ui.data.Result;
 
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NetworkServiceLayer {
 
-    static String API_BASE_URL = "http://demo1.stsm.co.in/api/App/";
+    static String API_BASE_URL = "http://demo1.stsm.co.in/";
 
     public static Object newInstance(Class className) {
-
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
         OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
         Retrofit.Builder builder = new Retrofit.Builder().baseUrl(API_BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create());
-        Retrofit retrofit = builder.client(httpClient.build()).build();
+        Retrofit retrofit = builder.client(client).build();
         return retrofit.create(className);
     }
 
--- app/src/main/java/com/app/alphasucess/service/RestServiceLayer.java
+++ app/src/main/java/com/app/alphasucess/service/RestServiceLayer.java
@@ -8,15 +8,15 @@ import retrofit2.http.POST;
 
 public interface RestServiceLayer
 {
-    @POST("/oauth/login")
+    @POST("/api/App/oauth/login")
     @FormUrlEncoded
     Call<Object> loginService(@Field("UserName")String uName,@Field("Password")String password,@Field("grant_type")String grant_type);
 
-    @POST("/oauth/ForgotPassword")
+    @POST("/api/App/oauth/ForgotPassword")
     @FormUrlEncoded
     Call<Object> forgotPassword(@Field("Phone")String uName);
 
-    @POST("/oauth/UserRegister")
+    @POST("/api/App/oauth/UserRegister")
     @FormUrlEncoded
     Call<Object> signUpApi(@Field("Email")String uEmail,@Field("Name")String uName,@Field("Password")String uPass,@Field("Phone")String uPhone,@Field("StateID") String uStateId,@Field("Address") String uAddress,@Field("isReffered") boolean isRefer);
 
