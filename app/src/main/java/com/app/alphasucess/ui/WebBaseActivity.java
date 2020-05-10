65c72a548c9d2758b9277b5485b91df1056420e5
ASHISH <ashishsharma@beusalons.com>
2020-05-10 13:12:37 +0530

    fddf

--- app/src/main/java/com/app/alphasucess/ui/WebBaseActivity.java
+++ app/src/main/java/com/app/alphasucess/ui/WebBaseActivity.java
@@ -37,6 +37,7 @@ public class WebBaseActivity extends BaseActivity {
 //        myWebView.loadDataWithBaseURL(null, htmlString, "text/html", "utf-8", null);
         final ImageView backBtnView = findViewById(R.id.backBtnView);
 
+
         initLoadResourceData();
         TextView header=findViewById(R.id.middleTitle);
         header.setText(bundle.getString("View-Name"));
