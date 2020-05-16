67722a8f48c65d055b0e082204a0375699bc71c4
ASHISH <ashishsharma@beusalons.com>
2020-05-17 01:17:35 +0530

    "home screen"

--- app/src/main/java/com/app/alphasucess/ui/tabui/home/HomeFragment.java
+++ app/src/main/java/com/app/alphasucess/ui/tabui/home/HomeFragment.java
@@ -9,6 +9,9 @@ import android.widget.TextView;
 import com.app.alphasucess.R;
 import com.app.alphasucess.ui.tabui.adapter.ExamAdapter;
 import com.app.alphasucess.ui.tabui.adapter.ExamData;
+import com.app.alphasucess.ui.tabui.home.adapter.LivecourseAdapter;
+import com.app.alphasucess.ui.tabui.home.adapter.LivecourseEdu;
+import com.app.alphasucess.ui.tabui.home.adapter.LivecourseVideo;
 import com.app.alphasucess.ui.tabui.test.adapters.QuestionCountAdapter;
 
 import java.util.ArrayList;
@@ -18,6 +21,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -27,21 +31,43 @@ import static androidx.recyclerview.widget.RecyclerView.HORIZONTAL;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-
+    RecyclerView recyclerView_onlinecourse,recyclerView_onlineeducation,recyclerView_onlinevideo;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        final RecyclerView recyclerView = root.findViewById(R.id.examRecyclerView);
-        initHomeExamView(recyclerView);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//        final TextView textView = root.findViewById(R.id.text_home);
+//        final RecyclerView recyclerView = root.findViewById(R.id.examRecyclerView);
+//        initHomeExamView(recyclerView);
+        /*homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
+        recyclerView_onlinecourse = (RecyclerView) root.findViewById(R.id.rcy__livecourses);
+        recyclerView_onlineeducation = (RecyclerView) root.findViewById(R.id.rcy_online_education);
+        recyclerView_onlinevideo = (RecyclerView) root.findViewById(R.id.rcy_online_video);
+
+
+        recyclerView_onlinevideo.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+
+
+        recyclerView_onlinecourse.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+
+
+        recyclerView_onlineeducation.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+
+
+
+        //set data and list adapter
+        LivecourseAdapter mAdapter = new LivecourseAdapter(getActivity());
+        LivecourseEdu livecourseEdu = new LivecourseEdu(getActivity());
+        LivecourseVideo livecourseVideo = new LivecourseVideo(getActivity());
+        recyclerView_onlinevideo.setAdapter(livecourseVideo);
+        recyclerView_onlinecourse.setAdapter(mAdapter);
+        recyclerView_onlineeducation.setAdapter(livecourseEdu);
         return root;
     }
 
---
+++ app/src/main/java/com/app/alphasucess/ui/tabui/home/adapter/LivecourseAdapter.java
@@ -0,0 +1,79 @@
+package com.app.alphasucess.ui.tabui.home.adapter;
+
+/**
+ * Created by Hrusikesh swain on 5/16/2020.
+ * Be U Salons
+ * hrusikeshswain@beusalons.com
+ */
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.app.alphasucess.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ashish Sharma on 1/4/2020.
+ * Be U Salons
+ * ashishsharma@beusalons.com
+ */
+public class LivecourseAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private Context ctx;
+    private ArrayList<String> categories;
+    private boolean isProducts;
+
+    public LivecourseAdapter(Context ctx) {
+        this.ctx = ctx;
+
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder vh;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_livecourses, parent, false);
+        vh = new OriginalViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return 6;
+    }
+
+    public class OriginalViewHolder extends RecyclerView.ViewHolder {
+        public ImageView image;
+        public TextView title;
+        public View lyt_parent;
+
+        public OriginalViewHolder(View v) {
+            super(v);
+           /* image = (ImageView) v.findViewById(R.id.img_service);
+            title = (TextView) v.findViewById(R.id.txt_service_name);
+            lyt_parent = v.findViewById(R.id.parent_layout);*/
+           /* Typeface lato_regular  = ResourcesCompat.getFont(ctx, R.font.lato_regular);
+            title.setTypeface(lato_regular);*/
+
+
+        }
+    }
+
+
+}
