60c8f3f6f1823f4c4cb9df2d6435f30cfdfa61ab
ASHISH <ashishsharma@beusalons.com>
2020-05-27 15:24:40 +0530

    "home screen"

--- app/src/main/java/com/app/alphasucess/ui/tabui/test/adapters/OnlineTestAdapter.java
+++ app/src/main/java/com/app/alphasucess/ui/tabui/test/adapters/OnlineTestAdapter.java
@@ -133,7 +133,7 @@ public class OnlineTestAdapter extends RecyclerView.Adapter {
 
         void bind(ArrayList<ExamData> allTestData) {
 
-            recyclerView.setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.HORIZONTAL, true));
+            recyclerView.setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.HORIZONTAL, false));
             ExamAdapter examAdapter = new ExamAdapter(mContext,allTestData);
             recyclerView.setAdapter(examAdapter);
         }
